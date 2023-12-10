765e791c5da519a6f1fd657c38706cdbb6965351
zhoulinhua <zhoulinhua>
2023-12-10 16:54:53 +0800

    默认变更列表

--- working-order-manage/src/main/java/com/woke/working/order/WorkingOrderManageApplication.java
+++ working-order-manage/src/main/java/com/woke/working/order/WorkingOrderManageApplication.java
@@ -5,8 +5,11 @@ import com.woke.working.web.annotation.EnableMq;
 import com.woke.working.web.annotation.EnableUserInfoTransmitter;
 import com.woke.working.web.annotation.EnableWebUtil;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.woke.working.order.dao")
@@ -20,4 +23,8 @@ public class WorkingOrderManageApplication {
         SpringApplication.run(WorkingOrderManageApplication.class, args);
     }
 
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
 }
--- working-web-manage/src/main/java/com/woke/working/web/WorkingWebManageApplication.java
+++ working-web-manage/src/main/java/com/woke/working/web/WorkingWebManageApplication.java
@@ -1,8 +1,11 @@
 package com.woke.working.web;
 
 import com.woke.working.web.annotation.EnableMq;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableMq
@@ -12,4 +15,9 @@ public class WorkingWebManageApplication {
         SpringApplication.run(WorkingWebManageApplication.class, args);
     }
 
+
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
 }
