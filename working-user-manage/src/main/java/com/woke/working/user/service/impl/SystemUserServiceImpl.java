30da36aafd169319fe1007239360f1a7d59d56a7
13537765632 <13537765632@163.com>
2023-12-14 23:57:38 +0800

    111111

--- working-user-manage/src/main/java/com/woke/working/user/service/impl/SystemUserServiceImpl.java
+++ working-user-manage/src/main/java/com/woke/working/user/service/impl/SystemUserServiceImpl.java
@@ -34,99 +34,99 @@ import java.util.Objects;
 @Service
 public class SystemUserServiceImpl implements SystemUserService {
 
-    @Autowired
-    private SystemUserDao systemUserDao;
-    @Autowired
-    private SystemUserRoleService systemUserRoleService;
-    @Autowired
-    private RSAEncrypt rsaEncrypt;
-    
-    @Autowired
-    private SystemUserRoleDao systemUserRoleDao;
+	@Autowired
+	private SystemUserDao systemUserDao;
+	@Autowired
+	private SystemUserRoleService systemUserRoleService;
+	@Autowired
+	private RSAEncrypt rsaEncrypt;
 
-    @Override
-    @Transactional(rollbackFor = Exception.class)
-    public ResponseVo add(SystemUserDTO systemUserDTO) {
-        // 查询用户账号是不是存在
-        Long userAccountCount = systemUserDao.selectCount(new LambdaQueryWrapper<SystemUser>()
-                .eq(SystemUser::getAccountNo, systemUserDTO.getAccountNo())
-                .eq(SystemUser::getStatus, StatusEnum.ENABLE.getCode()));
-        if (userAccountCount > 0L) {
-            throw new BusinessErrorException(BusinessMsgEnum.WORKING_USER_ACCOUNT_NO_EXIST);
-        }
-        SystemUser systemUser = new SystemUser();
-        BeanUtils.copyProperties(systemUserDTO, systemUser);
-        try {
-            systemUser.setPassword(rsaEncrypt.encryptDefault(systemUserDTO.getPassword()));
-        } catch (Exception e) {
-            throw new BusinessErrorException(BusinessMsgEnum.UNEXPECTED_EXCEPTION);
-        }
-        Date date = new Date();
-        systemUser.setCreateTime(date);
-        systemUser.setUpdateTime(date);
-        systemUserDao.insert(systemUser);
-        if (!StringUtils.isEmpty(systemUserDTO.getSelectedroles())) {
-        	String [] roles = systemUserDTO.getSelectedroles().split(",");
-        	for (String string : roles) {
-        		SystemUserRole systemUserRole = new SystemUserRole();
-                systemUserRole.setUserId(systemUser.getId());
-                systemUserRole.setRoleId(string);
-                systemUserRoleService.save(systemUserRole);
-			}
-        }
-        return ResponseVo.success();
-    }
-
-    @Override
-    @Transactional(rollbackFor = Exception.class)
-    public ResponseVo deleteUser(String id) {
-        SystemUser systemUser = systemUserDao.selectById(id);
-        if (Objects.isNull(systemUser)) {
-            throw new BusinessErrorException(BusinessMsgEnum.WORKING_USER_ACCOUNT_NO_NOT_EXIST);
-        }
-        systemUser.setStatus(false);
-        systemUserDao.deleteById(systemUser);
-        return ResponseVo.success();
-    }
-
-    @Override
-    public ResponseVo updateUser(SystemUserDTO systemUserDTO) {
-        SystemUser systemUser = systemUserDao.selectById(systemUserDTO.getId());
-        if (Objects.isNull(systemUser)) {
-            throw new BusinessErrorException(BusinessMsgEnum.WORKING_USER_ACCOUNT_NO_NOT_EXIST);
-        }
-        BeanUtils.copyProperties(systemUserDTO, systemUser);
-        systemUserDao.updateById(systemUser);
-        if (!StringUtils.isEmpty(systemUserDTO.getSelectedroles())) {
-        	systemUserRoleService.remove(new LambdaQueryWrapper<SystemUserRole>()
-                    .eq(SystemUserRole::getUserId, systemUser.getId()));
-        	String [] roles = systemUserDTO.getSelectedroles().split(",");
-        	for (String string : roles) {
-        		SystemUserRole systemUserRole = new SystemUserRole();
-                systemUserRole.setUserId(systemUser.getId());
-                systemUserRole.setRoleId(string);
-                systemUserRoleService.save(systemUserRole);
-			}
-        }
-        return ResponseVo.success();
-    }
-
-    @Override
-    public ResponseVo selectUserPage(SystemUserPageDTO systemUserPageDTO) {
-        Page<SystemUser> page = new Page<SystemUser>(systemUserPageDTO.getOffSet(), systemUserPageDTO.getPageSize());
-		IPage<SystemUser> pageList = systemUserDao.selectUserPage(page, systemUserPageDTO);
-        return ResponseVo.success(pageList);
-    }
-
-    @Override
-    public ResponseVo selectUserDetails(String id) {
-        return ResponseVo.success(systemUserDao.selectUserDetails(id));
-    }
+	@Autowired
+	private SystemUserRoleDao systemUserRoleDao;
 
 	@Override
-	public ResponseVo checkAccountNo(String id,String name) {
+	@Transactional(rollbackFor = Exception.class)
+	public ResponseVo add(SystemUserDTO systemUserDTO) {
+		// 查询用户账号是不是存在
+		Long userAccountCount = systemUserDao.selectCount(
+				new LambdaQueryWrapper<SystemUser>().eq(SystemUser::getAccountNo, systemUserDTO.getAccountNo())
+						.eq(SystemUser::getStatus, StatusEnum.ENABLE.getCode()));
+		if (userAccountCount > 0L) {
+			throw new BusinessErrorException(BusinessMsgEnum.WORKING_USER_ACCOUNT_NO_EXIST);
+		}
+		SystemUser systemUser = new SystemUser();
+		BeanUtils.copyProperties(systemUserDTO, systemUser);
+		try {
+			systemUser.setPassword(rsaEncrypt.encryptDefault(systemUserDTO.getPassword()));
+		} catch (Exception e) {
+			throw new BusinessErrorException(BusinessMsgEnum.UNEXPECTED_EXCEPTION);
+		}
+		Date date = new Date();
+		systemUser.setCreateTime(date);
+		systemUser.setUpdateTime(date);
+		systemUserDao.insert(systemUser);
+		if (!StringUtils.isEmpty(systemUserDTO.getSelectedroles())) {
+			String[] roles = systemUserDTO.getSelectedroles().split(",");
+			for (String string : roles) {
+				SystemUserRole systemUserRole = new SystemUserRole();
+				systemUserRole.setUserId(systemUser.getId());
+				systemUserRole.setRoleId(string);
+				systemUserRoleService.save(systemUserRole);
+			}
+		}
+		return ResponseVo.success();
+	}
+
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public ResponseVo deleteUser(String id) {
+		SystemUser systemUser = systemUserDao.selectById(id);
+		if (Objects.isNull(systemUser)) {
+			throw new BusinessErrorException(BusinessMsgEnum.WORKING_USER_ACCOUNT_NO_NOT_EXIST);
+		}
+		systemUser.setStatus(false);
+		systemUserDao.deleteById(systemUser);
+		return ResponseVo.success();
+	}
+
+	@Override
+	public ResponseVo updateUser(SystemUserDTO systemUserDTO) {
+		SystemUser systemUser = systemUserDao.selectById(systemUserDTO.getId());
+		if (Objects.isNull(systemUser)) {
+			throw new BusinessErrorException(BusinessMsgEnum.WORKING_USER_ACCOUNT_NO_NOT_EXIST);
+		}
+		BeanUtils.copyProperties(systemUserDTO, systemUser);
+		systemUserDao.updateById(systemUser);
+		if (!StringUtils.isEmpty(systemUserDTO.getSelectedroles())) {
+			systemUserRoleService
+					.remove(new LambdaQueryWrapper<SystemUserRole>().eq(SystemUserRole::getUserId, systemUser.getId()));
+			String[] roles = systemUserDTO.getSelectedroles().split(",");
+			for (String string : roles) {
+				SystemUserRole systemUserRole = new SystemUserRole();
+				systemUserRole.setUserId(systemUser.getId());
+				systemUserRole.setRoleId(string);
+				systemUserRoleService.save(systemUserRole);
+			}
+		}
+		return ResponseVo.success();
+	}
+
+	@Override
+	public ResponseVo selectUserPage(SystemUserPageDTO systemUserPageDTO) {
+		Page<SystemUser> page = new Page<SystemUser>(systemUserPageDTO.getOffSet(), systemUserPageDTO.getPageSize());
+		IPage<SystemUser> pageList = systemUserDao.selectUserPage(page, systemUserPageDTO);
+		return ResponseVo.success(pageList);
+	}
+
+	@Override
+	public ResponseVo selectUserDetails(String id) {
+		return ResponseVo.success(systemUserDao.selectUserDetails(id));
+	}
+
+	@Override
+	public ResponseVo checkAccountNo(String id, String name) {
 		SystemUser role = null;
-		if(!StringUtils.isEmpty(id)) {
+		if (!StringUtils.isEmpty(id)) {
 			role = systemUserDao.getAccountCodeById(id);
 		}
 		SystemUser newRole = systemUserDao.getAccountNoTenant(name);
