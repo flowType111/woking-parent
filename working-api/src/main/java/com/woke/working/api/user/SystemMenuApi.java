a65bc51505544933c8f914ff194a87a7937c930d
Administrator <Administrator@WIN-OGSG0J6LDLI>
2023-12-14 22:58:39 +0800

    1

--- working-api/src/main/java/com/woke/working/api/user/SystemMenuApi.java
+++ working-api/src/main/java/com/woke/working/api/user/SystemMenuApi.java
@@ -1,26 +1,29 @@
 package com.woke.working.api.user;
 
-import com.woke.working.common.dto.user.SystemMenuDTO;
+import com.woke.working.common.dto.user.SysPermissionDTO;
 import com.woke.working.common.dto.user.SystemMenuPageDTO;
 import com.woke.working.common.vo.ResponseVo;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/system/menu")
 public interface SystemMenuApi {
 
     @PostMapping("/addMenu")
-    ResponseVo addMenu(SystemMenuDTO systemMenuDTO);
+    ResponseVo addMenu(@RequestBody SysPermissionDTO permission);
 
-    @GetMapping("/deleteMenu")
+    @DeleteMapping("/deleteMenu")
     ResponseVo deleteMenu(String id);
 
     @PostMapping("/updateMenu")
-    ResponseVo updateMenu(SystemMenuDTO systemMenuDTO);
+    ResponseVo updateMenu(@RequestBody SysPermissionDTO permission);
 
     @PostMapping("/selectMenuPage")
     ResponseVo selectMenuPage(SystemMenuPageDTO systemMenuPageDTO);
@@ -39,4 +42,8 @@ public interface SystemMenuApi {
     
     @GetMapping("/queryRolePermission")
     ResponseVo queryRolePermission(String roleId);
+    
+    @GetMapping("/checkPermDuplication")
+	ResponseVo checkPermDuplication(@RequestParam(name = "id", required = false) String id,
+			@RequestParam(name = "url") String url, @RequestParam(name = "alwaysShow") Boolean alwaysShow);
 }
