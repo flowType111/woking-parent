b0284cbc5c20247264bab36b04b6ee185e20d767
13537765632 <13537765632@163.com>
2023-12-23 14:58:14 +0800

    支付配置联调

--- working-api/src/main/java/com/woke/working/api/common/PayConfigApi.java
+++ working-api/src/main/java/com/woke/working/api/common/PayConfigApi.java
@@ -3,6 +3,8 @@ package com.woke.working.api.common;
 import com.woke.working.common.dto.user.PayConfigDTO;
 import com.woke.working.common.dto.user.PayConfigPageDTO;
 import com.woke.working.common.vo.ResponseVo;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,7 +14,7 @@ public interface PayConfigApi {
     @RequestMapping("/addPayConfig")
     ResponseVo addPayConfig(PayConfigDTO payConfigDTO);
 
-    @GetMapping("/deletePayConfig")
+    @DeleteMapping("/deletePayConfig")
     ResponseVo deletePayConfig(String id);
 
     @RequestMapping("/updatePayConfig")
--- working-api/src/main/java/com/woke/working/api/order/PayOrderApi.java
+++ working-api/src/main/java/com/woke/working/api/order/PayOrderApi.java
@@ -2,11 +2,13 @@ package com.woke.working.api.order;
 
 import com.woke.working.common.dto.order.OrderPageDTO;
 import com.woke.working.common.vo.ResponseVo;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/order/payment")
 public interface PayOrderApi {
 
-    @RequestMapping("/selectOrderPage")
+	@PostMapping("/selectOrderPage")
     ResponseVo selectOrderPage(OrderPageDTO orderPageDTO);
 }
